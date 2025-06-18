3a92d60c126620dbf3995e1924a2eefa9c25b439
Preston TW <preston_tunnell_wilson@brown.edu>
2025-06-17 23:44:14 -0400

    Initial commit
    After running `nix develop` and `cargo run`

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils,  ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            rustup
+          ] ++ lib.optionals stdenv.isDarwin [libiconv llvmPackages.bintools];
+        };
+      });
+}
+
